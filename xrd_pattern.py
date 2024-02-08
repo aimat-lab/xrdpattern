1155b67b7d08d14aa0c47675aa8f38a0ee147df6
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-08 22:05:28 +0100

    xrdpattern: Implemented reading input file to python list

--- xrd_pattern.py
+++ xrd_pattern.py
@@ -1,14 +1,14 @@
 from typing import Optional
-from numpy import ndarray as NumpyArr
 from file_io import get_repr, Formats
-
+import re
 
 # -------------------------------------------
+
 
 class XrdPattern:
     def __init__(self, filepath : Optional[str] = None):
         self.wave_length_angstrom : Optional[float] = None
-        self.np_degree_repr : Optional[NumpyArr] = None
+        self.degree_over_intensity : list[(float,float)] = []
         self.source_file : Optional[bytes] = None
 
         if filepath:
@@ -17,9 +17,24 @@ class XrdPattern:
 
     def import_from_file(self,filepath : str):
         _ = self
-        print(f'Beginning conversion')
         xy_content = get_repr(input_path=filepath, xrd_format=Formats.XSYG)
-        print(f'Xy content is {xy_content}')
+        rows = [row for row in xy_content.split('\n') if not row.strip() == '']
+        header_pattern = r'# column_1\tcolumn_2'
+
+        try:
+            header_match = re.findall(pattern=header_pattern, string=xy_content)[0]
+        except Exception as e:
+            raise ValueError(f"Could not find header matching pattern \"{header_pattern}\" in file {filepath}. Error: {str(e)}")
+
+        header_row_index = rows.index(header_match)
+        data_rows = rows[header_row_index+1:]
+        for row in data_rows:
+
+            deg_str, intensity_str = row.split()
+            deg, intensity = float(deg_str), float(intensity_str)
+            self.degree_over_intensity.append((deg, intensity))
+
+        print(self.degree_over_intensity)
 
 
     def export_to_file(self):
@@ -34,10 +49,10 @@ class XrdPattern:
         pass
 
     def get_np_repr(self):
-        if not self.np_degree_repr:
+        if not self.degree_over_intensity:
             raise ValueError(f"Numpy array is None")
 
-        return self.np_degree_repr
+        return self.degree_over_intensity
 
 if __name__ == "__main__":
     xrd_pattern = XrdPattern(filepath="/home/daniel/aimat/pxrd_data/processed/example_files/asdf.raw")
