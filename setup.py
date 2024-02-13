31349f67acaa1e7934a9f06ff36f04d681777f12
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-13 04:39:48 +0100

    General: exported xylib to seperate repo and pypi package

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
-import os
-
 from setuptools import setup
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-
-requirements += [f"xylib-py @ file://localhost/{os.getcwd()}/xylib"]
 
 setup(
     name='xrdpattern',
