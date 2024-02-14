aca9ca0956f9c857c570eea59ccbb243514c64a1
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-02-14 03:49:59 +0100

    xrd_file_io: Solved unresolved reference warnings

--- xrdpattern/__init__.py
+++ xrdpattern/__init__.py
@@ -1,3 +1,3 @@
 from xrdpattern.xrd_types import XrdPattern, XrdPatternDB
-from xrdpattern.xrd_logger import initialize_logger, log_xrd_info
+from xrdpattern.xrd_logger import log_xrd_info
 from xrdpattern.xrd_file_io import find_all_parsable_files
