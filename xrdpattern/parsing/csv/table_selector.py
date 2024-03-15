fbd307438e9f17e8c736395bf6908cf5fb21bb75
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-15 14:43:39 +0100

    parsing/csv: Extracted table selection into dedicated TableSelector module

---
+++ xrdpattern/parsing/csv/table_selector.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Callable, Optional
+
+from xrdpattern.parsing.csv.tables import TextTable
+
+
+@dataclass
+class Index:
+    row : int
+    col : int
+
+
+@dataclass
+class Region:
+    upper_left : Index
+    lower_right : Index
+
+
+@dataclass
+class TableSelector:
+    table : TextTable
+    discriminator : Callable[[str], bool]
+
+    def get_lower_right_subtable(self) -> Optional[Region]:
+        region = self.get_lower_right_square()
+        if region:
+            region = self.get_upwards_expansion(region=region)
+            region=  self.get_leftwards_expansion(region=region)
+        return region
+
+    def get_upwards_expansion(self, region : Region) -> Region:
+        upper_left = region.upper_left
+        lower_right = region.lower_right
+        while True:
+            partial_row = self.table.get_row(row=upper_left.row-1, start_col=upper_left.col, end_col=lower_right.col)
+            is_ok = all([self.discriminator(x) for x in partial_row])
+            if is_ok:
+                upper_left.row -=1
+            else:
+                break
+        return Region(upper_left=upper_left, lower_right=lower_right)
+
+
+    def get_leftwards_expansion(self, region : Region) -> Region:
+        upper_left = region.upper_left
+        lower_right = region.lower_right
+        while True:
+            partial_col = self.table.get_col(col=upper_left.col-1, start_row=upper_left.row, end_row=lower_right.row)
+            is_ok = all([self.discriminator(x) for x in partial_col])
+            if is_ok:
+                upper_left.col -=1
+            else:
+                break
+        return Region(upper_left=upper_left, lower_right=lower_right)
+
+
+    def get_lower_right_square(self) -> Optional[Region]:
+        upper_left = None
+        new_upper_left = self.table.get_lower_right_index()
+        while True:
+            partial_row = self.table.get_row(new_upper_left.row, new_upper_left.col)
+            failed_on_row = not all([self.discriminator(x) for x in partial_row])
+
+            partial_col = self.table.get_col(new_upper_left.col, new_upper_left.row)
+            failed_on_col = not all([self.discriminator(x) for x in partial_col])
+            if not failed_on_row and not failed_on_col:
+                upper_left = Index(row=new_upper_left.row,col=new_upper_left.col)
+                new_upper_left.row -= 1
+                new_upper_left.col -= 1
+            else:
+                break
+
+        if not upper_left:
+            region = None
+        else:
+            region = Region(upper_left=upper_left, lower_right=self.table.get_lower_right_index())
+        return region
--- xrdpattern/parsing/csv/tables.py
+++ xrdpattern/parsing/csv/tables.py
@@ -1,20 +1,9 @@
 from __future__ import annotations
-
 from dataclasses import dataclass
-from typing import Callable
+from typing import Optional
+from xrdpattern.parsing.csv.table_selector import Index
 
-
-@dataclass
-class Index:
-    row : int
-    col : int
-
-
-@dataclass
-class Region:
-    upper_left : Index
-    lower_right : Index
-
+# -------------------------------------------
 
 @dataclass
 class TextTable:
@@ -27,59 +16,27 @@ class TextTable:
         if not all([len(row) == len(self.content[0]) for row in self.content]):
             raise ValueError("All rows must have the same length")
 
-    def get_lower_right_subtable(self, discriminator : Callable[[str], bool]) -> Region:
-        upper_left = None
-        new_upper_left = self.get_lower_right_index()
-        while True:
-            partial_row = self.get_row(new_upper_left.row, new_upper_left.col)
-            failed_on_row = not all([discriminator(x) for x in partial_row])
-
-            partial_col = self.get_col(new_upper_left.col, new_upper_left.row)
-            failed_on_col = not all([discriminator(x) for x in partial_col])
-            if not failed_on_row and not failed_on_col:
-                upper_left = Index(row=new_upper_left.row,col=new_upper_left.col)
-                new_upper_left.row -= 1
-                new_upper_left.col -= 1
-            else:
-                break
-
-        while True:
-            partial_col = self.get_col(col=upper_left.col-1, start_row=upper_left.row)
-            is_ok = all([discriminator(x) for x in partial_col])
-            if is_ok:
-                upper_left.col -=1
-            else:
-                break
-
-        while True:
-            partial_col = self.get_row(row=upper_left.row-1, start_col=upper_left.col)
-            is_ok = all([discriminator(x) for x in partial_col])
-            if is_ok:
-                upper_left.row -=1
-            else:
-                break
-
-        return Region(upper_left=upper_left, lower_right=self.get_lower_right_index())
-
     def get_lower_right_index(self) -> Index:
-        return Index(self.get_row_count()-1, self.get_row_len()-1)
+        return Index(self.get_row_count() - 1, self.get_row_len() - 1)
 
 
     # -------------------------------------------
     # get rows/cols
 
-    def get_row(self, row : int, start_col : int = 0):
-        return self.content[row][start_col:]
+    def get_row(self, row : int, start_col : int = 0, end_col : Optional[int] = None):
+        end_col = end_col if not end_col is None else self.get_row_len()-1
+        return [self.content[row][col] for col in range(start_col, end_col+1)]
 
-    def get_col(self, col : int, start_row : int = 0):
-        return [self.content[row][col] for row in range(start_row, self.get_row_count())]
+
+    def get_col(self, col : int, start_row : int = 0, end_row : Optional[int]  = None):
+        end_row = end_row if not end_row is None else self.get_row_count()-1
+        return [self.content[row][col] for row in range(start_row, end_row+1)]
 
     def get_row_count(self) -> int:
         return len(self.content)
 
     def get_row_len(self) -> int:
         return len(self.content[0])
-
 
 
 
@@ -101,42 +58,3 @@ class NumericalTable:
     def get_header(self, index : int) -> str:
         return self.headers[index]
 
-
-if __name__ == "__main__":
-    # Define three separate tables of sizes 2x2, 3x3, and 5x5 respectively, with blocks of '1's
-
-    # 2x2 Table
-    table_2x2 = [
-        ["1", "1"],
-        ["1", "0"]
-    ]
-
-    # 3x3 Table
-    table_3x3 = [
-        ["1", "0", "0"],
-        ["1", "1", "1"],
-        ["0", "1", "1"]
-    ]
-
-    table4x4 = [
-        ["1", "1", "1", "0"],
-        ["1", "1", "0", "0"],
-        ["1", "0", "0", "0"],
-        ["1", "1", "1", "1"]
-    ]
-
-    # 5x5 Table
-    table_5x5 = [
-        ["1", "1", "0", "0", "0"],
-        ["1", "1", "1", "0", "0"],
-        ["0", "1", "1", "1", "1"],
-        ["0", "1", "1", "1", "1"],
-        ["0", "1", "1", "1", "1"]
-    ]
-
-    test_disc = lambda x: x == '1'
-    text_table = TextTable(table_5x5)
-    # text_table.get_maximal_region(test_disc)
-
-    print(text_table.get_lower_right_subtable(discriminator=test_disc))
-    # text_table.get_maximal_region(discriminator=test_disc)
