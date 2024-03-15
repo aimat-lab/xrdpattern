0ff362300b11cb81ca48bf71fd33a3a92dbf335d
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-15 21:49:14 +0100

    test: Included finding data check in CsvTests

--- tests/csv/t_read_csv.py
+++ tests/csv/t_read_csv.py
@@ -11,15 +11,20 @@ class TestCsvPreprocessor(Unittest):
         test_dirpath = os.path.dirname(os.path.abspath(__file__))
         self.vertical_csv_path = os.path.join(test_dirpath,'vertical.csv')
         self.horizontal_csv_path = os.path.join(test_dirpath,'horizontal.csv')
+        self.first_row = [1.79272, 1.80218, 1.81164, 1.8211, 1.83056, 1.84002, 1.84948, 1.85894, 1.8684]
 
     def test_read_csv_vertical(self):
-        # Read vertically oriented CSV and validate the structure or content
         numerical_table = self.preprocessor.read_csv(self.vertical_csv_path, Orientation.VERTICAL)
-        # self.assertIsInstance(numerical_table, type(expected_table))  # Check type
-        # self.assertEqual(numerical_table, expected_table)  # Check content
+        expected_set = set(self.first_row)
+        actual_set = set(numerical_table.data[0])
+        self.assertTrue(expected_set.issubset(actual_set))
 
     def test_read_csv_horizontal(self):
         numerical_table = self.preprocessor.read_csv(self.horizontal_csv_path, Orientation.HORIZONTAL)
+        expected_set = set(self.first_row)
+        actual_set = set(numerical_table.data[0])
+        self.assertTrue(expected_set.issubset(actual_set))
+
 
 if __name__ == '__main__':
     unittest.main()
