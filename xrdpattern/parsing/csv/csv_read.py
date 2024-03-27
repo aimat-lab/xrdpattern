9e0dcee011d8d3786109f736da60f38bb542bfcf
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-03-27 13:03:51 +0100

    parsing: Introduced from_csv in XrdParser

--- xrdpattern/parsing/csv/csv_read.py
+++ xrdpattern/parsing/csv/csv_read.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from hollarek.abstract import SelectableEnum
-from xrdpattern.pattern import XAxisType
+from xrdpattern.pattern import XAxisType, XrdPattern
 from .table_selector import TableSelector, NumericalTable, TextTable
 
 # -------------------------------------------
@@ -11,38 +11,48 @@ class Orientation(SelectableEnum):
     HORIZONTAL = 'horizontal'
 
 
+class Seperator(SelectableEnum):
+    COMMA = ','
+    SEMICOLON = ';'
+    TAB = '\t'
+
+
 @dataclass
 class CsvScheme:
     x_axis_type : XAxisType
     pattern_dimension : Orientation
+    seperator: str = ','
     header_lines : int = 1
+
+    def __post_init__(self):
+        if not self.seperator in [',', ';', '\t']:
+            raise ValueError(f"Invalid seperator: {self.seperator}")
 
     @classmethod
     def from_manual(cls) -> CsvScheme:
         print(f'Please specify csv scheme')
         x_axis_type = XAxisType.from_manual_query()
         pattern_dimension = Orientation.from_manual_query()
-        return CsvScheme(x_axis_type=x_axis_type, pattern_dimension=pattern_dimension)
+        seperator = Seperator.from_manual_query()
+        return CsvScheme(x_axis_type=x_axis_type, pattern_dimension=pattern_dimension, seperator=seperator)
 
 
 class CsvReader:
-    def __init__(self, seperator : str):
-        if not seperator in [',', ';', '\t']:
-            raise ValueError(f"Invalid seperator: {seperator}")
-        self.seperator : str = seperator
+    def __init__(self, csv_scheme : CsvScheme):
+        self.csv_scheme : CsvScheme = csv_scheme
 
 
-    def read_csv(self, fpath: str, outerDimensionAxis: Orientation = Orientation.VERTICAL) -> NumericalTable:
+    def read_csv(self, fpath: str) -> XrdPattern:
         data = []
         with open(fpath, 'r', newline='') as infile:
             for line in infile:
-                row = [item.strip() for item in line.strip().split(self.seperator)]
+                row = [item.strip() for item in line.strip().split(self.csv_scheme.seperator)]
                 if row and any(item for item in row):
                     data.append(row)
 
-        if outerDimensionAxis == Orientation.HORIZONTAL:
+        if self.csv_scheme.pattern_dimension == Orientation.HORIZONTAL:
             data = [list(col) for col in zip(*data)]
         table = TextTable(data)
         print(f'fpath,row, col length = {fpath} {table.get_row_count()}, {table.get_row_len()}')
-        return TableSelector.get_numerical_subtable(table=table)
+        # return TableSelector.get_numerical_subtable(table=table)
 
--- xrdpattern/parsing/xrdparser.py
+++ xrdpattern/parsing/xrdparser.py
@@ -7,7 +7,7 @@ from hollarek.fsys import FsysNode
 from xrdpattern.pattern import XrdPattern, Metadata
 from xrdpattern.database import XrdPatternDB
 from .data_files import XrdFormat, Formats, get_xylib_repr
-from .csv import CsvScheme
+from .csv import CsvScheme, CsvReader
 
 # -------------------------------------------
 
@@ -23,9 +23,9 @@ class XrdParser:
     def __init__(self, parser_options : ParserOptions = ParserOptions()):
         if parser_options.select_suffixes is None:
             self.select_formats : list[str] = Formats.get_allowed_suffixes()
-        self.csv_scheme : Optional[CsvScheme] =  parser_options.csv_scheme
         self.format_hint : Optional[XrdFormat] = parser_options.format_hint
         self.default_wavelength_angstr : Optional[float] = parser_options.default_wavelength_angstr
+        self.default_csv_reader : Optional[CsvReader] = CsvReader(parser_options.csv_scheme)
 
     # -------------------------------------------
     # pattern
@@ -41,11 +41,7 @@ class XrdParser:
                 format_hint = Formats.get_format(suffix=suffix)
             xrd_pattern = [self.from_data_file(fpath=fpath, format_hint=format_hint)]
         elif suffix == 'csv':
-            csv_scheme = self.csv_scheme
-            if not csv_scheme:
-                print(f'No csv scheme specified for {fpath}')
-                csv_scheme = CsvScheme.from_manual()
-            xrd_pattern = self.from_csv(fpath=fpath, csv_scheme=csv_scheme)
+            xrd_pattern = self.from_csv(fpath=fpath)
         else:
             raise ValueError(f"Unable to determine format of file {fpath} without format hint or file extension")
         for pattern in xrd_pattern:
@@ -79,9 +75,17 @@ class XrdParser:
         return XrdPattern(twotheta_to_intensity=twotheta_to_intensity, metadata=metadata)
 
 
-    @staticmethod
-    def from_csv(fpath : str, csv_scheme : CsvScheme) -> list[XrdPattern]:
-        raise NotImplementedError(f"CSV parsing not implemented yet")
+    def from_csv(self, fpath : str, csv_scheme : Optional[CsvScheme] = None) -> list[XrdPattern]:
+        csv_reader = None
+        if csv_scheme:
+            csv_reader = CsvReader(csv_scheme)
+        if self.default_csv_reader:
+            csv_reader = self.default_csv_reader
+        if not csv_reader:
+            print(f'No CSV scheme specified in either argument or as default value of xrdparser.'
+                  f'Please specify csv scheme for {fpath} manually:')
+            csv_reader = CsvReader(CsvScheme.from_manual())
+        return csv_reader.read_csv(fpath=fpath)
 
     # -------------------------------------------
     # pattern database
