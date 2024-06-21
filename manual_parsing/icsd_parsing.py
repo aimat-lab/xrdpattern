acd8678cf399d02c516851b350a976d1fd9352bd
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-06-21 02:16:46 +0200

    manual_parsing: Added icsd pattern parsing code

---
+++ manual_parsing/icsd_parsing.py
@@ -0,0 +1,39 @@
+import json
+import os
+import tempfile
+import uuid
+
+from xrdpattern.core import PowderExperiment
+from xrdpattern.pattern import XrdPattern
+
+icsd_json_path = '/home/daniel/Drive/data/workspace/icsd/dataset.json'
+keywords = ['fullprof', 'rietveld', 'powder']
+save_dir = '/home/daniel/Drive/data/workspace/icsd_extracted'
+
+with open(icsd_json_path, 'r') as f:
+    the_dict = json.loads(f.read())
+print(f'Loaded icsd dataset')
+
+
+the_label = PowderExperiment.from_file()
+
+for num, content in the_dict.items():
+    lower_content = content.lower()
+    if not any([word in lower_content for word in keywords]):
+        print(f'Could not find any matching keywords in cif file number {num}')
+        continue
+    else:
+        print(f'Found keyword matching powder diffraction in cif file number {num}! \n'
+              f'Starting pattern extraction')
+
+    try:
+        tmp_fpath = tempfile.mktemp(suffix='.cif')
+        with open(tmp_fpath, 'w') as f:
+            f.write(content)
+            pattern = XrdPattern.load(fpath=tmp_fpath)
+            fpath = os.path.join(save_dir, str(uuid.uuid4()))
+            pattern.save(fpath=fpath)
+        print(f'Successfullly extracted pattern from cif and wrote information to {fpath}')
+    except Exception as e:
+        print(f'An error occured during extraction: {e}')
+
