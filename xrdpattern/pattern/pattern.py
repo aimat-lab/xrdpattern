e0a5a8d63ec0812cf02bac6e14e425d4b51de8b1
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-07-14 13:31:42 +0200

    General: Deleted debugging printing

--- xrdpattern/pattern/pattern.py
+++ xrdpattern/pattern/pattern.py
@@ -89,14 +89,9 @@ class XrdPattern(PatternData):
         return self.label.is_simulated
 
     def to_tensor_pair(self) -> tuple[torch.Tensor, torch.Tensor]:
-        now = time.time()
-
         labels = self.label.to_tensor()
         _, intensities = self.get_pattern_data()
         intensities = torch.tensor(intensities)
-
-        time_taken = time.time() - now
-        print(f'Time taken = {time_taken}')
 
         return intensities, labels
 
