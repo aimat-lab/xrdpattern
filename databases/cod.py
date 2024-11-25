082264cb8b0bb225ca87154d98b08f28863ef43f
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-11-25 05:28:10 +0100

    databases: Re-added try-except block

--- databases/cod.py
+++ databases/cod.py
@@ -18,23 +18,24 @@ def write_cod(json_fpath : str, out_dirpath : str):
     base_url = 'https://www.crystallography.net/cod'
 
     for cod_id, value in the_dict.items():
-        num = cod_id.split('/')[-1]
-        request_url = f'{base_url}/{num}.cif'
-        cif_content = requests.get(url=request_url).content.decode()
+        try:
+            num = cod_id.split('/')[-1]
+            request_url = f'{base_url}/{num}.cif'
+            cif_content = requests.get(url=request_url).content.decode()
 
-        temp_fpath = SaveManager.get_temp_fpath()
-        with open(temp_fpath, 'w') as f:
-            f.write(cif_content)
+            temp_fpath = SaveManager.get_tmp_fpath()
+            with open(temp_fpath, 'w') as f:
+                f.write(cif_content)
 
-        cif_parser = CifParser()
-        pattern = cif_parser.extract_pattern(fpath=temp_fpath)
+            cif_parser = CifParser()
+            pattern = cif_parser.extract_pattern(fpath=temp_fpath)
 
-        fname = f"COD_{value['id']}"
-        save_fpath = os.path.join(out_dirpath, f'{fname}.json')
-        pattern.save(fpath=save_fpath, force_overwrite=True)
-        print(f'Successfully parsed structure number {value["id"]} and saved file at {save_fpath}')
-
-
+            fname = f"COD_{value['id']}"
+            save_fpath = os.path.join(out_dirpath, f'{fname}.json')
+            pattern.save(fpath=save_fpath, force_overwrite=True)
+            print(f'Successfully parsed structure number {value["id"]} and saved file at {save_fpath}')
+        except BaseException as e:
+            print(f'Failed to extract COD pattern due to error {e}')
 
 if __name__ == "__main__":
     j_fpath = '/home/daniel/aimat/opXRD/raw/coudert_hardiagon_0/data/extracted_data.json'
