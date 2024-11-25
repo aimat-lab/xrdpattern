b83c669e9e211a4077fdb2e14f09c089bf8bb1d5
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-11-25 11:33:03 +0100

    setup.py: Fixed inclusion of example fiiles

--- setup.py
+++ setup.py
@@ -44,5 +44,5 @@ setup(name='xrdpattern',
       py_modules=['xylib'],
       cmdclass={'build': CustomBuild},
       package_data = {'xrdpattern.crystal.atomic_constants': ['*'],'xrdpattern.crystal.cifs': ['*'],
-                      'xrdpattern.databases.examples': ['*', 'datafolder/*']}
+                      'xrdpattern.parsing.examples': ['*', 'datafolder/*']}
 )
