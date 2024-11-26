e4b8ef1d4dbf66b8a059e344e30de51c65ebc519
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-11-26 02:47:59 +0100

    databases/opxrd: Added parsing of individual contributions

--- databases/opxrd.py
+++ databases/opxrd.py
@@ -1,9 +1,9 @@
 import os
 from dataclasses import dataclass
+from typing import Optional
 
-from xrdpattern.parsing.stoe import StoeParser
-from xrdpattern.pattern import PatternDB,XrdPattern
-
+from xrdpattern.pattern import PatternDB
+# -------------------------------------------
 
 @dataclass
 class ContributorInfo:
@@ -16,12 +16,12 @@ class DatabaseProcessor:
         self.raw_dirpath : str = os.path.join(root_dirpath, 'raw')
         self.processed_dirpath : str = os.path.join(root_dirpath, 'processed')
 
-    def process_contribution(self, dirname: str):
+    def process_contribution(self, dirname: str, selected_suffixes : Optional[list[str]] = None):
         contribution_dirpath = os.path.join(self.raw_dirpath, dirname)
         info = self.get_contributor_info(contribution_dirpath=contribution_dirpath)
 
         data_dirpath = os.path.join(contribution_dirpath, 'data')
-        pattern_db = PatternDB.load(dirpath=data_dirpath, selected_suffixes=['raw'])
+        pattern_db = PatternDB.load(dirpath=data_dirpath, selected_suffixes=selected_suffixes)
         for p in pattern_db.patterns:
             p.metadata.contributor_name = info.name_advisor
             p.metadata.institution = info.institution
@@ -47,12 +47,18 @@ class DatabaseProcessor:
         )
         return personal_info
 
+    # ---------------------------------------
+    # Parsing individual contributions
+
+    def parse_INT(self):
+        self.process_contribution(dirname='breitung_schweidler_0', selected_suffixes=['raw'])
+        self.process_contribution(dirname='breitung_schweidler_1', selected_suffixes=['raw'])
+
+    def parse_CNRS(self):
+        self.process_contribution(dirname='coudert_hardiagon_0', selected_suffixes=['json'])
+
 
 if __name__ == "__main__":
     processor = DatabaseProcessor(root_dirpath='/home/daniel/aimat/opXRD/')
-    processor.process_contribution(dirname='breitung_schweidler_1')
-
-    # fpath= '/home/daniel/aimat/opXRD/raw/breitung_schweidler_1/data/daten_gallium/Nithin/NS-366-Rec6_back_combined.raw'
-    # pattern = XrdPattern.load(fpath=fpath)
-    # pattern.save(fpath=f'stoe.json')
+    processor.parse_INT()
 
