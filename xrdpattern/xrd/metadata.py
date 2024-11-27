dc1c97bd442d8b65a9e2618622225d7ccff0cb5f
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-11-27 04:04:14 +0100

    xrd: Added xrdpattern_version to Metadata fields

--- xrdpattern/xrd/metadata.py
+++ xrdpattern/xrd/metadata.py
@@ -12,16 +12,13 @@ def get_library_version(library_name):
 
 @dataclass
 class Metadata(JsonDataclass):
-    filename : Optional[str] = None
-    institution : Optional[str] = None
-    contributor_name : Optional[str] = None
-    original_file_format : Optional[str] = None
+    filename: Optional[str] = None
+    institution: Optional[str] = None
+    contributor_name: Optional[str] = None
+    original_file_format: Optional[str] = None
     measurement_date: Optional[str] = None
     tags: list[str] = field(default_factory=list)
-
-
-    def __post_init__(self):
-        self.xrdpattern_version : str = get_library_version(library_name='xrdpattern')
+    xrdpattern_version: str = field(default_factory=lambda: get_library_version('xrdpattern'))
 
     def __eq__(self, other : Metadata):
         s1, s2 = self.to_str(), other.to_str()
