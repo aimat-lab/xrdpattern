cb0fc8d5af8bb6275e7e6f1857eaf2c885f69018
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-12-02 15:49:06 +0100

    processors: Split contribution defintion and opXRDProcessor into seperate modules

--- databases/processors/opxrd.py
+++ databases/processors/opxrd.py
@@ -117,35 +117,3 @@ class ContributionProcessor:
         if not os.path.isdir(out_dirpath):
             os.makedirs(out_dirpath)
         pattern_db.save(dirpath=out_dirpath,label_groups=label_groups)
-
-
-
-class OpXRDProcessor(ContributionProcessor):
-    def parse_INT(self):
-        self.process(dirname='breitung_schweidler_0', selected_suffixes=['raw'])
-        self.process(dirname='breitung_schweidler_1', selected_suffixes=['raw'])
-
-    def parse_CNRS(self):
-        self.process(dirname='coudert_hardiagon_0', selected_suffixes=['json'])
-
-    def parse_USC(self):
-        self.process(dirname='hodge_alwen_0', xray_info=self.cu_xray)
-        self.process(dirname='hodge_alwen_1', xray_info=self.cu_xray)
-
-    def parse_LBNL(self):
-        self.process(dirname='sutter-fella_heymans_0', selected_suffixes=['xlsx'], csv_orientation=Orientation.VERTICAL, label_groups=True)
-
-    def parse_EMPA(self):
-        self.process(dirname='siol_wieczorek_0', xray_info=self.cu_xray)
-        self.process(dirname='siol_zhuk_0', xray_info=self.cu_xray)
-
-    def parse_IKFT(self):
-        self.process(dirname='wolf_wolf_0', xray_info=self.cu_xray)
-
-    def parse_HKUST(self):
-        self.process(dirname='zhang_cao_0', use_cif_labels=True,selected_suffixes=['txt'], xray_info=self.cu_xray)
-
-
-if __name__ == "__main__":
-    processor = OpXRDProcessor(root_dirpath='/home/daniel/aimat/data/opXRD/')
-    processor.parse_LBNL()
