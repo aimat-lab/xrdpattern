30017443db7d1623c9250f3e7e6c824643b05153
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-12-11 05:11:41 +0100

    databases/tools: updted SpacegroupConverter.to_int to handle rruff/international formulas

--- databases/tools/spg_converter.py
+++ databases/tools/spg_converter.py
@@ -22,30 +22,32 @@ def get_formula_to_int(fpath):
     return sg_dict
 
 dirpath = os.path.dirname(__file__)
-formula_to_int = get_formula_to_int(fpath=os.path.join(dirpath, 'spg_formulas.txt'))
+rruff_formula_to_int = get_formula_to_int(fpath=os.path.join(dirpath, 'spg_formulas.txt'))
 
 
 class SpacegroupConverter:
     @staticmethod
     def to_int(spg_formula : str) -> int:
-        spg_formula = spg_formula.replace('-', '')
-        spg_formula = spg_formula.replace('_', '')
-
-        return formula_to_int[spg_formula]
+        if spg_formula in rruff_formula_to_int:
+            spg_int = rruff_formula_to_int[spg_formula]
+        else:
+            spg =  SpaceGroup(int_symbol=spg_formula)
+            spg_int = spg.int_number
+        return spg_int
 
     @staticmethod
     def to_formula(spg_int : int, mathmode : bool = False):
         sg = SpaceGroup.from_int_number(spg_int)
         symbol = sg.symbol
-        symbol = symbol.replace(' ', '')
         if mathmode:
+            symbol = symbol.replace(' ', '')
             symbol = symbol.replace('-', '\\text{-}')
         return symbol
 
     @staticmethod
     def get_all_formulas() -> list[str]:
-        return list(formula_to_int.keys())
+        return list(rruff_formula_to_int.keys())
 
 if __name__ == "__main__":
     # print(SpacegroupConverter.to_int(spg_formula='Ia-3'))
-    print(f'formula to int dict = {formula_to_int}')
+    print(f'formula to int dict = {rruff_formula_to_int}')
