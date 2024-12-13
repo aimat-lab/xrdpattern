202b70141a7ebdf937edf98958a8e36c0ab0d1ca
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-12-13 13:50:43 +0100

    patternDB: Introduced colorbar placement option in .show_histogram

--- xrdpattern/pattern/db.py
+++ xrdpattern/pattern/db.py
@@ -7,11 +7,9 @@ from typing import Optional
 import matplotlib.colors
 import matplotlib.gridspec as gridspec
 import numpy as np
-import seaborn as sns
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 from databases.tools.spg_converter import SpacegroupConverter
 from holytools.logging import LoggerFactory
@@ -147,7 +145,7 @@ class PatternDB:
                 break
 
 
-    def show_histograms(self, save_fpath : Optional[str] = None):
+    def show_histograms(self, save_fpath : Optional[str] = None, attach_colorbar : bool = True):
         fig = plt.figure(figsize=(12,8))
 
         figure = gridspec.GridSpec(nrows=2, ncols=1, figure=fig, hspace=0.35)
@@ -160,13 +158,21 @@ class PatternDB:
         ax3 = fig.add_subplot(lower_half[:, 0])
         self.define_recorded_angles_ax(patterns=self.patterns, ax=ax3)
 
-        lower_half_right = lower_half[1].subgridspec(nrows=3, ncols=3, width_ratios=[3,3,4])
-        ax4 = fig.add_subplot(lower_half_right[1:, :2]) # scaatter
-        ax5 = fig.add_subplot(lower_half_right[:1, :2], sharex=ax4)  # Above
-        ax6 = fig.add_subplot(lower_half_right[1:, 2:], sharey=ax4)  # Right
-        ax7 = fig.add_subplot(lower_half_right[:1, 2:])
-        ax7.axis('off')
-        self.define_density_ax(patterns=self.patterns, density_ax=ax4, top_marginal=ax5, right_marginal=ax6, cmap_ax=ax7)
+        if attach_colorbar:
+            lower_half_right = lower_half[1].subgridspec(nrows=3, ncols=3, width_ratios=[3, 3, 4])
+            ax4 = fig.add_subplot(lower_half_right[1:, :2])  # scaatter
+            ax5 = fig.add_subplot(lower_half_right[:1, :2], sharex=ax4)  # Above
+            ax6 = fig.add_subplot(lower_half_right[1:, 2:], sharey=ax4)  # Right
+            ax7 = fig.add_subplot(lower_half_right[:1, 2:])
+            ax7.axis('off')
+        else:
+            lower_half_right = lower_half[1].subgridspec(nrows=3, ncols=4, width_ratios=[4,3,3,3])
+            ax4 = fig.add_subplot(lower_half_right[1:, 1:3]) # scatter
+            ax5 = fig.add_subplot(lower_half_right[:1, 1:3], sharex=ax4)  # Above
+            ax6 = fig.add_subplot(lower_half_right[1:, 3:4], sharey=ax4)  # Right
+            ax7 = fig.add_subplot(lower_half_right[:4, :1])
+
+        self.defined_start_end_ax(patterns=self.patterns, density_ax=ax4, top_marginal=ax5, right_marginal=ax6, cmap_ax=ax7, attach_colorbar=attach_colorbar)
 
         if save_fpath:
             plt.savefig(save_fpath)
@@ -195,13 +201,12 @@ class PatternDB:
         ax.set_ylabel(f'No. patterns')
 
     @staticmethod
-    def define_density_ax(patterns : list[XrdPattern], density_ax : Axes, top_marginal : Axes, right_marginal : Axes, cmap_ax):
+    def defined_start_end_ax(patterns : list[XrdPattern], density_ax : Axes, top_marginal : Axes, right_marginal : Axes, cmap_ax : Axes, attach_colorbar : bool):
         start_data = get_valid_values(patterns=patterns, attr='startval')
         end_data = get_valid_values(patterns=patterns, attr='endval')
         start_angle_range = (0,60)
         end_angle_range = (0,180)
 
-        # sns.kdeplot(x=start_data, y=end_data, fill=True, ax=density_ax)
         # noinspection PyTypeChecker
         h = density_ax.hist2d(start_data,end_data, bins=(10,10), range=[list(start_angle_range),list(end_angle_range)], norm=matplotlib.colors.LogNorm())
         density_ax.set_xlabel(r'Smallest recorded $2\theta$ [$^\circ$]')
@@ -209,22 +214,33 @@ class PatternDB:
         density_ax.set_xlim(start_angle_range)
         density_ax.set_ylim(end_angle_range)
 
-        divider = make_axes_locatable(density_ax)
-        cax = divider.append_axes('right', size='5%', pad=0.0)
-        plt.colorbar(h[3], cax=cax, orientation='vertical')
+        if attach_colorbar:
+            divider = make_axes_locatable(density_ax)
+            cax = divider.append_axes('right', size='5%', pad=0.0)
+            plt.colorbar(h[3], cax=cax, orientation='vertical')
+
+        else:
+            plt.colorbar(h[3], cax=cmap_ax, orientation='vertical', location='left')
+            cmap_ax.set_ylabel(f'No. patterns')
 
         top_marginal.hist(start_data, bins=np.linspace(*start_angle_range, num=10), edgecolor='black')
         top_marginal.set_title(f'(c)')
         top_marginal.set_yscale('log')
         top_marginal.tick_params(axis="x", labelbottom=False, which='both', bottom=False)
 
-        divider = make_axes_locatable(top_marginal)
-        cax = divider.append_axes('right', size='5%', pad=0.0)
-        cax.axis('off')
+        if attach_colorbar:
+            divider = make_axes_locatable(top_marginal)
+            cax = divider.append_axes('right', size='5%', pad=0.0)
+            cax.axis('off')
 
-        divider = make_axes_locatable(right_marginal)
-        cax = divider.append_axes('left', size='15%', pad=0.0)
-        cax.axis('off')
+            divider = make_axes_locatable(right_marginal)
+            cax = divider.append_axes('left', size='15%', pad=0.0)
+            cax.axis('off')
+
+        else:
+            divider = make_axes_locatable(cmap_ax)
+            cax = divider.append_axes('right', size=0.8, pad=0.0)
+            cax.axis('off')
 
         right_marginal.hist(end_data, bins=np.linspace(*end_angle_range, num=10), orientation='horizontal', edgecolor='black')
         right_marginal.set_xscale('log')
